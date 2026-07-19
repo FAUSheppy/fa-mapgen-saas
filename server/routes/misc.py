6ccd141ba691167dfd104534eab918712c570018
Yannik Schmidt <sheppy@atlantishq.de>
2026-07-19 20:10:13 +0200

    feat: open stats endpoint

--- server/routes/misc.py
+++ server/routes/misc.py
@@ -2,7 +2,7 @@
 import flask
 from flask import Blueprint, jsonify
 
-from sqlalchemy import and_, func
+from sqlalchemy import and_, func, select
 from sqlalchemy.orm import joinedload
 
 import datetime
@@ -12,6 +12,8 @@ import uuid
 from database.db_import import db
 from database.RequestQueue import RequestQueue
 from database.MapVote import MapVote
+from database.Map import Map
+from database.User import User
 
 import utils.flask_wrappers
 import utils.curators
@@ -22,6 +24,41 @@ bp = Blueprint("misc", __name__)
 def get_queue_size():
     count = db.session.query(RequestQueue).filter(RequestQueue.finished==False).count()
     return flask.jsonify({"count": count })
+
+
+@bp.route("/stats", methods=["GET"])
+def status():
+    stats = db.session.execute(
+        select(
+            select(func.count())
+            .select_from(RequestQueue)
+            .where(RequestQueue.finished.is_(False))
+            .scalar_subquery()
+            .label("request_queue"),
+
+            select(func.count())
+            .select_from(Map)
+            .scalar_subquery()
+            .label("maps"),
+
+            select(func.count())
+            .select_from(MapVote)
+            .scalar_subquery()
+            .label("map_votes"),
+
+            select(func.count())
+            .select_from(User)
+            .scalar_subquery()
+            .label("users"),
+        )
+    ).one()
+
+    return flask.jsonify({
+        "request_queue": stats.request_queue,
+        "maps": stats.maps,
+        "map_votes": stats.map_votes,
+        "users": stats.users,
+    })
 
 @bp.route("/whoami", methods=["GET"])
 @utils.flask_wrappers.with_username()
